2588042bd38cb16d674430e9e203ab8ebd2eca31
Koby <kobysysouvanh@gmail.com>
2023-06-12 10:46:44 -0400

    Created Airbnb App

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+import { Nunito } from "next/font/google";
+
+const font = Nunito({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
